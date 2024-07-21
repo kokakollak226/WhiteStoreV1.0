583bd3e89932b4efb9fe3da61d9c7c03ffb8d9ed
Konstantin <kostya2004god159@gmail.com>
2024-07-21 16:04:44 +0600

    stable version

--- app/handlers.py
+++ app/handlers.py
@@ -11,27 +11,32 @@ router = Router()
 class standgold(StatesGroup):
     gold = State()
     bank = State()
-    translate = State()
+    verify = State()
 
 @router.message(CommandStart())
 async def cmd_start(message: Message, state: FSMContext):
     state.clear
-    await message.answer(f'Здравствуйте {message.from_user.first_name}', reply_markup=kb.main)
+    await message.answer(f'👋Здравствуйте {message.from_user.first_name},\n 🏠Главное меню.\n'
+                        f'🔢Для взаимодействия с ботом используй клавиатуру.\n'
+                        f'⚡️ Для покупки голды перейди в раздел «💵 Купить».\n'
+                        f'📖 Если у тебя возникли вопросы, обращайся в поддержку.', reply_markup=kb.main)
     
     
-@router.message(F.text == 'Главное меню')
+@router.message(F.text == '🏠Главное меню')
 async def cmd_start(message: Message, state: FSMContext):
     state.clear
-    await message.answer(f'Здравствуйте {message.from_user.first_name}', reply_markup=kb.main)
+    await message.answer(f'👋Здравствуйте {message.from_user.first_name},\n 🏠Главное меню.\n'
+                        f'🔢Для взаимодействия с ботом используй клавиатуру.\n'
+                        f'⚡️ Для покупки голды перейди в раздел «💵 Купить».\n'
+                        f'📖 Если у тебя возникли вопросы, обращайся в поддержку.', reply_markup=kb.main)
     
 
 
-@router.message(F.text == 'Купить')
+@router.message(F.text == '💵Купить')
 async def buy(message: Message, state: FSMContext):
     await state.set_state(standgold.gold)
-    await message.answer(f'🍯 Чтобы купить голду, введи в чат сумму на которую хочешь пополнить баланс. \n 💡 Например: 100', reply_markup=kb.menu)
+    await message.answer(f'🍯 Чтобы купить голду, введи в чат сумму ₽ на которую хочешь пополнить баланс. \n 💡 Например: 100', reply_markup=kb.menu)
     
-
     
 @router.message(standgold.gold)
 async def sum(message: Message, state: FSMContext):    
@@ -40,60 +45,112 @@ async def sum(message: Message, state: FSMContext):
         rub = int(golds)
         golda = round(golds / 0.66, 2)
         await state.update_data(gold=message.text)
-        await message.answer(f'📝 За {rub} ₽ ты получаешь {golda} голды. Для пополнения баланса выбери наиболее удобный тебе способ оплаты:', reply_markup=kb.pay) 
+        await message.answer(f'📝 За {rub} ₽ ты получаешь {golda} голды. Для пополнения баланса выбери наиболее удобный тебе способ оплаты:', reply_markup=kb.bank) 
         await state.set_state(standgold.bank)
     except Exception:
         message.answer('Введите целое число')
-    return golda, rub
+    
+@router.callback_query(F.data == 'Back')
+async def Back(callback: CallbackQuery, state: FSMContext):
+    data = await state.get_data()
+    golda = int(data['gold'])
+    await callback.message.answer(f'📝 За {golda} ₽ ты получаешь {round(golda / 0.66, 2)} голды. Для пополнения баланса выбери наиболее удобный тебе способ оплаты:', reply_markup=kb.bank)
+    await callback.answer('Вы вернулись назад')
+    await callback.message.delete()
 
-@router.callback_query(F.data == 'translate' and standgold.bank)
-async def cataloge(callback: CallbackQuery, state: FSMContext):
-    await state.set_state(standgold.translate)
-    await callback.answer('Выберите способ перевода')
-    await callback.message.answer('Выберите способ перевода', reply_markup=kb.bank)
+@router.callback_query(F.data == 'Edit')
+async def Back(callback: CallbackQuery, state: FSMContext):
+    data = await state.get_data()
+    golda = int(data['gold'])
+    await callback.message.answer(f'📝 За {golda} ₽ ты получаешь {round(golda / 0.66, 2)} голды. Для пополнения баланса выбери наиболее удобный тебе способ оплаты:', reply_markup=kb.bank)
+    await callback.answer('Выберите способ оплаты')
     await callback.message.delete()
 
 @router.callback_query(F.data == 'SBP')
-async def SBP(callback: CallbackQuery, state: FSMContext):
+async def SBP(callback: CallbackQuery, state:FSMContext):
     await callback.answer('Действуйте по инструкции')
     data = await state.get_data()
     golda = int(data['gold'])
-    await callback.message.answer(f'Отправьте скриншот перевода, {data['gold']} и получите {round(golda / 0.66, 2)} ваше Имя и Отчёство +79087976609 Константин.К Тинькофф')
+    await callback.message.answer(f'📱Номер для перевода по СБП⚪️: +79087976609\n'
+                                  f'💳Банк Тинькофф(Т.Банк)\n'
+                                  f'❗️Получатель -Константин Александрович К.\n'
+                                  f'💰 Сумма: {golda} ₽\n'
+                                  f'🍯 Игровая комиссия рынка на нас.\n'
+                                  f'♻️Вам на аккаунт придет ровно: {round(golda / 0.66, 2)}G\n\n'
+                                  f'📸 После оплаты, нажмите «✅ Я перевел»', reply_markup=kb.Verify)
     await callback.message.delete()
     
 
 @router.callback_query(F.data == 'Sberbank')
-async def SBP(callback: CallbackQuery):
+async def Sberbank(callback: CallbackQuery, state:FSMContext):
     await callback.answer('Действуйте по инструкции')
-    await callback.message.answer(f'💳Карта: 2202202013287409 ❗️Получатель -Константин Алекандрович К. 💰 Сумма: {rub} ₽ 🍯 Игровая комиссия рынка на нас. Вам на аккаунт придет ровно: {golda} 📸 После оплаты, нажмите ✅ Я перевел')
+    data = await state.get_data()
+    golda = int(data['gold'])
+    await callback.message.answer(f'💳Карта🟢: 2202202013277409\n'
+                                  f'❗️Получатель -Константин Александрович К.\n'
+                                  f'💰 Сумма: {golda} ₽\n'
+                                  f'🍯 Игровая комиссия рынка на нас.\n'
+                                  f'♻️Вам на аккаунт придет ровно: {round(golda / 0.66, 2)}G\n\n'
+                                  f'📸 После оплаты, нажмите «✅ Я перевел»', reply_markup=kb.Verify)
     await callback.message.delete()
 
 @router.callback_query(F.data == 'Tinkoff')
-async def SBP(callback: CallbackQuery):
+async def Tinkoff(callback: CallbackQuery, state:FSMContext):
     await callback.answer('Действуйте по инструкции')
-    await callback.message.answer('Отправьте скриншот перевода, ваше Имя и Отчёство +79087976609 Константин.К Тинькофф(Т.Банк)')
+    data = await state.get_data()
+    golda = int(data['gold'])
+    await callback.message.answer(f'💳Карта🟡: 2200700817593386\n'
+                                  f'❗️Получатель -Константин Александрович К.\n'
+                                  f'💰 Сумма: {golda} ₽\n'
+                                  f'🍯 Игровая комиссия рынка на нас.\n'
+                                  f'♻️Вам на аккаунт придет ровно: {round(golda / 0.66, 2)}G\n\n'
+                                  f'📸 После оплаты, нажмите «✅ Я перевел»', reply_markup=kb.Verify)
     await callback.message.delete()
 
 @router.callback_query(F.data == 'Alfa')
-async def SBP(callback: CallbackQuery):
+async def Alfa(callback: CallbackQuery, state:FSMContext):
     await callback.answer('Действуйте по инструкции')
-    await callback.message.answer('Отправьте скриншот перевода, ваше Имя и Отчёство +79087976609 Константин.К Альфа Банк')
+    data = await state.get_data()
+    golda = int(data['gold'])
+    await callback.message.answer(f'💳Карта🔴: 2200150818007889\n'
+                                  f'❗️Получатель -Константин Александрович К.\n'
+                                  f'💰 Сумма: {golda} ₽\n'
+                                  f'🍯 Игровая комиссия рынка на нас.\n'
+                                  f'♻️Вам на аккаунт придет ровно: {round(golda / 0.66, 2)}G\n\n'
+                                  f'📸 После оплаты, нажмите «✅ Я перевел»', reply_markup=kb.Verify)
     await callback.message.delete()
 
 @router.callback_query(F.data == 'Vtb')
-async def SBP(callback: CallbackQuery):
+async def Vtb(callback: CallbackQuery, state:FSMContext):
     await callback.answer('Действуйте по инструкции')
-    await callback.message.answer('Отправьте скриншот перевода, ваше Имя и Отчёство +79087976609 Константин.К Втб Банк')
+    data = await state.get_data()
+    golda = int(data['gold'])
+    await callback.message.answer(f'💳Карта🔵: 2200246001639031\n'
+                                  f'❗️Получатель -Константин Александрович К.\n'
+                                  f'💰 Сумма: {golda} ₽\n'
+                                  f'🍯 Игровая комиссия рынка на нас.\n'
+                                  f'♻️Вам на аккаунт придет ровно: {round(golda / 0.66, 2)}G\n\n'
+                                  f'📸 После оплаты, нажмите «✅ Я перевел»', reply_markup=kb.Verify)
     await callback.message.delete()
 
-@router.callback_query(F.data == 'Qiwi')
-async def SBP(callback: CallbackQuery):
+@router.callback_query(F.data == 'Verify')
+async def verify(callback: CallbackQuery, state: FSMContext):
+    #await state.set_state(standgold.verify)
     await callback.answer('Действуйте по инструкции')
-    await callback.message.answer('Отправьте скриншот перевода, ваше Имя и Отчёство +79087976609 Константин.К Qiwi Кошелек')
+    await callback.message.answer('📸Скиньте сюда в чат скриншот перевода')
+    await callback.message.answer('⁉️ Важно: не обрезайте скриншот, на нем должно быть видно дату, время и получателя.')
     await callback.message.delete()
 
-@router.callback_query(F.data == 'Ymoney')
-async def SBP(callback: CallbackQuery):
+@router.callback_query(F.data == 'Problem')
+async def problem(callback: CallbackQuery, state: FSMContext):
     await callback.answer('Действуйте по инструкции')
-    await callback.message.answer('В разработке')
+    await callback.message.answer(f'⁉️ Если у вас не получается перевести,' 
+                                  f'попробуйте выбрать другой способ оплаты,\n'
+                                  f'💡например «⚪️СБП | Другой банк».\n\n'
+                                  f'Если проблема сохранилась, напишите нам ⤵️', reply_markup=kb.Faq)
     await callback.message.delete()
+
+
+@router.message()
+async def send_echo(message: Message):
+    await message.answer(f'🤖я не понимаю вас, если появились проблемы с работой бота нажмите /start')
--- app/keyboards.py
+++ app/keyboards.py
@@ -3,10 +3,10 @@ from aiogram.types import (ReplyKeyboardMarkup, KeyboardButton,
 
 
 main = ReplyKeyboardMarkup(keyboard=[
-    [KeyboardButton(text='Купить'), KeyboardButton(text='Продать')],
-    [KeyboardButton(text='Игры')],
-    [KeyboardButton(text='Курс'), KeyboardButton(text='Новости')],
-    [KeyboardButton(text='Отзывы')]
+    [KeyboardButton(text='💵Купить'), KeyboardButton(text='⚡️Вывести')],
+    [KeyboardButton(text='🎮Игры')],
+    [KeyboardButton(text='🔢Посчитать'), KeyboardButton(text='🍿Новости')],
+    [KeyboardButton(text='📝Отзывы')],[KeyboardButton(text='📖Поддержка')]
 ], 
 resize_keyboard=True, input_field_placeholder='Привет, нажми кнопку интересующую тебя'
 )
@@ -18,11 +18,20 @@ pay = InlineKeyboardMarkup(inline_keyboard=[
 bank = InlineKeyboardMarkup(inline_keyboard=[
     [InlineKeyboardButton(text='Сбербанк🟢', callback_data='Sberbank'), InlineKeyboardButton(text='Тинькофф🟡', callback_data='Tinkoff')],
     [InlineKeyboardButton(text='Альфа🔴', callback_data='Alfa'),InlineKeyboardButton(text='ВТБ🔵', callback_data='Vtb')],
-    [InlineKeyboardButton(text='СБП⚪️', callback_data='SBP')],
-    [InlineKeyboardButton(text='QIWI🟠', callback_data='Qiwi'), InlineKeyboardButton(text='ЮMoney🟣', callback_data='Ymoney')]
+    [InlineKeyboardButton(text='СБП⚪️', callback_data='SBP')]
 ])
 
 
 menu = ReplyKeyboardMarkup(keyboard=[
-    [KeyboardButton(text='Главное меню')]
+    [KeyboardButton(text='🏠Главное меню')]
 ], resize_keyboard=True)
+
+Verify = InlineKeyboardMarkup(inline_keyboard=[
+    [InlineKeyboardButton(text='✅ Я перевел', callback_data='Verify')], 
+    [InlineKeyboardButton(text='🚫Проблема с оплатой', callback_data='Problem')],
+    [InlineKeyboardButton(text='⬅️Изменить способ оплаты', callback_data='Edit')]
+])
+
+Faq = InlineKeyboardMarkup(inline_keyboard=[
+    [InlineKeyboardButton(text='⬅️Назад', callback_data='Back'), InlineKeyboardButton(text='📝Поддержка', callback_data='Faq')]
+])
